e36300beaab9b70cb4131c9caa6c515fa5eb143f
atousley <annette.tousley@gmail.com>
2019-01-26 19:04:57 -0600

    firebase auth register login logout and roughed out pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,54 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MenuPage } from '../pages/menu/menu';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { NewPage } from '../pages/new/new';
+
+import { EmojiPickerModule } from '@ionic-tools/emoji-picker';
+
+import { FirebaseService } from '../pages/services/firebase.service';
+import { AuthService } from '../pages/services/auth.service';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environment/environment';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuPage,
+    LoginPage,
+    RegisterPage,
+    NewPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuPage,
+    LoginPage,
+    RegisterPage,
+    NewPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    EmojiPickerModule,
+    FirebaseService,
+    AuthService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RegisterPage } from '../register/register';
+import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goLoginPage(){
+    this.navCtrl.push(LoginPage);
+  }
+
+  goRegisterPage(){
+    this.navCtrl.push(RegisterPage);
   }
 
 }
