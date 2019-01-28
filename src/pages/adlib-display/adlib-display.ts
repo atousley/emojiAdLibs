0de216c26508f310295d9d93ea244fd0ca71a419
atousley <annette.tousley@gmail.com>
2019-01-28 11:38:27 -0600

    cleaned up display and added mock vars

--- src/pages/adlib-display/adlib-display.ts
+++ src/pages/adlib-display/adlib-display.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { FirebaseService } from '../services/firebase.service';
+import { AuthService } from '../services/auth.service';
 
+import { MenuPage } from '../menu/menu';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the AdlibDisplayPage page.
@@ -19,12 +22,25 @@ import { FirebaseService } from '../services/firebase.service';
 export class AdlibDisplayPage {
 
   adlibId: string;
-  adlib1Data: Object = {};
+  adlib1Data: any;
+
+  toggled1: boolean = false;
+  toggled2: boolean = false;
+  toggled3: boolean = false;
+  toggled4: boolean = false;
+  toggled5: boolean = false;
+
+  emojitext1: string = "emoji1";
+  emojitext2: string = "emoji2";
+  emojitext3: string = "emoji3";
+  emojitext4: string = "emoji4";
+  emojitext5: string = "emoji5";
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private firebaseService: FirebaseService
+    private firebaseService: FirebaseService,
+    private authService: AuthService
   ) {
   }
 
@@ -39,11 +55,28 @@ export class AdlibDisplayPage {
 
     this.firebaseService.getAdlib1Data(this.adlibId)
     .then(emojis => {
+      //console.log(emojis);
+
       this.adlib1Data = emojis;
       console.log(this.adlib1Data);
+
       //console.log(this.adlib1Data._document.data.internalValue.root);
       //console.log(this.adlib1Data._document.data.internalValue.root.value);
+    })
+  }
 
+  setEmojiText(){
+
+  }
+
+  goMenuPage(){
+    this.navCtrl.push(MenuPage);
+  }
+
+  logout(){
+    this.authService.doLogout()
+    .then(res => {
+      this.navCtrl.push(HomePage);
     })
   }
 
